9306da59cd930ee2172284d74a9f6dd3b733e05f
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-15 17:44:27 +0530

    Created test server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const nike=require('./nike');
+const app = express();
+const port = 3005;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get("/test", (req, res) => {
+    res.json({data:nike});
+    });
+app.listen(port, () => {
+  console.log(`Server listening at port: ${port}`);
+});
---
+++ nike.js
@@ -0,0 +1,39 @@
+const popularsales = {
+  title: "nike",
+  items: [
+    {
+      id: "0p0x1",
+      title: "Nike Addapt BB 2.0",
+      text: "MEN Running Shoes",
+      rating: "4.9",
+      btn: "Buy Now",
+      img: "./assets/nike-air-red.png",
+      price: "200",
+      color: "from-blue-600 to-blue-500",
+      shadow: "shadow-lg shadow-blue-500",
+    },
+    {
+      id: "0p0x2",
+      title: "Nike Martine Rose",
+      text: "MEN Running Shoes",
+      rating: "4.5",
+      btn: "Buy Now",
+      img: "./assets/nike-adapt-bb.png",
+      price: "200",
+      color: "from-red-500 to-rose-500",
+      shadow: "shadow-lg shadow-rose-500",
+    },
+    {
+      id: "0p0x3",
+      title: "Nike Smart Shoe 2.0",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "./assets/nike-adapt-bb-smart.png",
+      price: "200",
+      color: "from-violet-500 to-indigo-500",
+      shadow: "shadow-lg shadow-violet-500",
+    },
+  ],
+};
+module.exports = popularsales;
