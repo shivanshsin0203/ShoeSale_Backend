df05fe3eaa8002c3bd1821b446bad9d4503750a3
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-16 20:53:18 +0530

    Added sort mechanism

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const nike=require('./nike');
-const adidas=require('./addidas');
-const puma=require('./puma'); 
+const nike = require("./nike");
+const adidas = require("./addidas");
+const puma = require("./puma");
 const app = express();
 const port = 3005;
 const bodyParser = require("body-parser");
@@ -11,9 +11,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.get("/test", (req, res) => {
-    res.json({data:adidas});
-    });
+app.get("/initial", (req, res) => {
+  res.json({ data: adidas });
+});
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  const allBrands = [].concat(nike.items, adidas.items, puma.items);
+  const sortOption = req.body.sortOption || "lowToHigh";
+  console.log(req.body.sortOption);
+  const sortedItems = sortItems(allBrands, sortOption);
+
+  res.json({ data: sortedItems });
+});
+function sortItems(items, sortOption) {
+  return items.sort((a, b) => {
+    const priceA = parseFloat(a.price);
+    const priceB = parseFloat(b.price);
+
+    if (sortOption === 'highToLow') {
+      return priceB - priceA;
+    } else {
+      return priceA - priceB;
+    }
+  });
+}
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`);
 });
